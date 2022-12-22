d3d7ffdf674088c7bfe8bfd149c80f7f3f660196
Mark Katerberg <katerberg@fastmail.fm>
2022-12-22 15:00:27 -0600

    Moves drawing into tiles

--- src/game.ts
+++ src/game.ts
@@ -8,17 +8,13 @@ export class Game {
     for (let w = 0; w < width; w++) {
       this.tiles[w] = [];
       for (let h = 0; h < height; h++) {
-        const coordinates = `${Math.floor(w / 2) - Math.floor(h / 2)},${h}`;
-        const color = 'rgba(255, 255, 255, 1)';
         // x position is divided by two to get display position
         // y position is accurate
         if (h % 2 && w % 2) {
           this.tiles[w][h] = new Tile(w, h);
-          globalThis.display.draw(w, h, coordinates, null, color);
         }
         if (h % 2 === 0 && w % 2 === 0) {
           this.tiles[w][h] = new Tile(w, h);
-          globalThis.display.draw(w, h, coordinates, null, color);
         }
       }
     }
--- src/tile.ts
+++ src/tile.ts
@@ -6,5 +6,8 @@ export class Tile {
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
+    const color = 'rgba(255, 255, 255, 1)';
+    const coordinates = `${Math.floor(x / 2) - Math.floor(y / 2)},${y}`;
+    globalThis.display.draw(x, y, coordinates, null, color);
   }
 }

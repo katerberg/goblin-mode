ff167d0492e7d04d12d9eb460433b6a9be700a4e
Mark Katerberg <katerberg@fastmail.fm>
2022-12-22 14:48:09 -0600

    First classes

---
+++ src/globals.d.ts
@@ -0,0 +1,6 @@
+import * as ROT from 'rot-js';
+
+declare global {
+  //eslint-disable-next-line no-var
+  var display: ROT.Display;
+}
---
+++ src/tile.ts
@@ -0,0 +1,10 @@
+export class Tile {
+  public x;
+
+  public y;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}

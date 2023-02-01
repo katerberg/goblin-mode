16d81af5654be4d48dc48628f8ad5ca6246faf8b
Mark Katerberg <katerberg@fastmail.fm>
2023-02-01 14:31:48 -0600

    Intro messsage

--- src/sceneManager.ts
+++ src/sceneManager.ts
@@ -1,4 +1,4 @@
-import {clearCanvas, drawSomeText, isDebug} from './utils';
+import {clearCanvas, drawSomeText, isDebug, wrapText} from './utils';
 
 export class SceneManager {
   private gameStartCallback: () => Promise<boolean>;
@@ -41,7 +41,23 @@ export class SceneManager {
 
   init(): void {
     this.hideGameElements();
-    setTimeout(() => drawSomeText('Goblin Mode', undefined, undefined, {textAlign: 'center', color: 'orange'}), 1);
+    setTimeout(() => {
+      drawSomeText('Goblin Mode', undefined, undefined, {textAlign: 'center', color: 'orange'});
+      const ctx = (globalThis.display.getContainer() as HTMLCanvasElement)?.getContext('2d');
+      if (ctx) {
+        const description =
+          'Dwarves have delved too deep into your most recent home. Now you must lead your goblins out of the mine, leveling them up as they sneak past the multitudes of annoying humans before the monster behind you arrives to claim your hovel.';
+        wrapText(ctx, description, 20, ctx.canvas.height / 2 + 50, ctx.canvas.width - 100, 40).forEach(
+          ([text, x, y]) => {
+            drawSomeText(text, x, y, {
+              textAlign: 'left',
+              color: 'white',
+              font: '24px serif',
+            });
+          },
+        );
+      }
+    }, 1);
     globalThis.gameElement.ontouchstart = this.startGame.bind(this);
   }
 
